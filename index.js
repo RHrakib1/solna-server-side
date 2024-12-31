449e22f90c67c4b74b192392c8c9d8a158cdc4ef
RHrakib1 <rakibhasanmd457@gmail.com>
2024-12-31 23:43:45 +0600

    3rd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const corsOption = {
-    origin: ['https://solo-prokect.web.app/','http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', ],
+    origin: ['https://solo-prokect.web.app','http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
